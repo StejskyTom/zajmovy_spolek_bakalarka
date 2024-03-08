13f8c65186f04b943e6b9fb871760313c91b3c3a
StejskyTom <tomas@alchy.cz>
2024-03-08 10:53:32 +0100

    project update

--- public/assets/scroll.js
+++ public/assets/scroll.js
@@ -1,14 +1,14 @@
 $(window).on('scroll', e => {
     if($(window).scrollTop() < 150) {
         console.log('sdf');
-        $('a').removeClass('active')
+        $('a:not(.lang)').removeClass('active')
         $('a#home').addClass('active');
     } else {
         $('h2').each(function() {
             if($(this).offset().top - 150 < $(window).scrollTop()) {
                 let id = '#' + $(this).closest('article').data('section');
                 console.log(id);
-                $('a').removeClass('active')
+                $('a:not(.lang)').removeClass('active')
                 $('a' + id).addClass('active')
             }
         })
